9f1cc636f59438a487931775e6e6977217fae84d
DiAleKir <Nameless12628@gmail.com>
2025-08-25 22:14:39 +0300

    обновлён get_object.py - добавлены тесты на поиск объектов
    добавлен тест поиска всех объектов
    добавлен тест поиска нескольких объектов по id
    обновлён endpoints - добавлены новые эндпоинты
    обновлён base_methods.py - добавлен метод проверки, что вернулся не пустой response
    обновлён base_methods.py - добавлен метод для проверки, что вернулись элементы с нужными id

--- endpoints/endpoints.py
+++ endpoints/endpoints.py
@@ -1,8 +1,10 @@
 BASE_URL = 'https://api.restful-api.dev/objects'
 
-class Endpoints:
 
+class Endpoints:
     ADD_OBJECT = BASE_URL
     GET_OBJECT_SINGLE = lambda self, obj_id: f'{BASE_URL}/{obj_id}'
+    GET_ALL_OBJECTS = BASE_URL
+    GET_OBJECTS_BY_IDS = lambda self, id1, id2: f'{BASE_URL}?id={id1}&id={id2}'
     UPDATE_OBJECT = lambda self, obj_id: f'{BASE_URL}/{obj_id}'
     DELETE_OBJECT = lambda self, obj_id: f'{BASE_URL}/{obj_id}'
