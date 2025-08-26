1da1a418f2043f9ac92eb13fa68f85614dfdf11e
DiAleKir <Nameless12628@gmail.com>
2025-08-26 17:41:50 +0300

    добавлен patch тест в test_update_object.py
    добавлен payload для частичного обновления объекта
    добавлен метод частичного обновления объекта

--- tests/test_update_object.py
+++ tests/test_update_object.py
@@ -1,8 +1,16 @@
+import pytest
+
 from tests.base_test import BaseTest
 
 
 class TestUpdateObject(BaseTest):
 
+    @pytest.mark.usefixtures("object_id")
     def test_update_object(self, object_id):
         self.update_object.update_object(object_id)
+        self.update_object.check_response_is_200()
+
+    @pytest.mark.usefixtures("object_id")
+    def test_patch_object(self, object_id):
+        self.update_object.patch_object(object_id)
         self.update_object.check_response_is_200()
